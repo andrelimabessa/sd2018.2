2808369f3d302ef06e89312ef8dc97e0c2a02dec
diegoteixeiramarques <diegodtm@live.com>
2018-10-27 11:43:20 -0300

    Alteracao no import para consolidar a criacao do arquivo clienteModelo que permite herenca para clienteControle

---
+++ Campo-Minado-ClienteServidor/ClienteArquivos/clienteControle.py
@@ -0,0 +1,30 @@
+import clienteModelo
+
+class CampoMinado(clienteModelo.CampoMinado):
+    def __init__(self, fieldSize):
+        super().__init__(fieldSize)
+
+    '''  Atributos de Herança '''    
+        #cleanField : matriz
+        #dict : dicionario
+
+    def showCleanField(self):
+        print('\n')
+        self.close = '--' * len(self.cleanField) * 2
+        print('\n', '', self.close)
+        for line in self.cleanField:
+            print(end=' | ')
+            for column in line:
+                print(column, end=' | ')
+                #self.close = '--' * len(line) * 2
+            print('\n', '', self.close)
+
+    def updateDict(self, answer):
+        x = -1
+        sizeField = range(len(self.cleanField))
+        for l in sizeField:
+            y = 0
+            x = x + 1
+            for c in sizeField:
+                self.cleanField[x][y] = answer[(x, y)]          
+                y = y + 1
