ed5f1bd62cbc3ed692f7ecdad798a0868a4dffb9
Davi Costa <davispcosta@gmail.com>
2018-11-11 22:57:44 -0300

    sending...

---
+++ trabVP2/questao01/client/client.py
@@ -0,0 +1,85 @@
+import socket
+from datetime import datetime
+from controller import Minefield
+import ast
+import subprocess
+
+ENCODE = "UTF-8"
+HOST = '127.0.0.1'   
+PORT = 5000          
+MAX_BYTES = 65535    
+
+
+def client():
+    sizeField = int(input("Informe o tamanho total:"))
+    numberBobm = int(input("Informe quantidade de bombas:"))
+    
+    cm = CampoMinado(sizeField)
+    cm.dict['column'] = numberBobm
+    cm.dict['line'] = sizeField
+    
+    request = cm.dict
+    request = str(request)
+    request = request.encode(ENCODE)				    
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+    dest = (HOST, PORT)                                      
+    sock.sendto(request, dest)                                 
+    answer, address = sock.recvfrom(MAX_BYTES)    # Recebendo dados
+
+    answer = answer.decode(ENCODE)                  # Convertendo dados de BASE64 para UTF-8
+    answer = ast.literal_eval(answer)
+
+    if(answer['altered'] == True):
+        cm.updateDict(answer)
+        cm.showCleanField()
+        answer['msg']
+        print('Falta decobrir', answer['freeAreas'], 'áreas')
+
+    sock.close()
+    
+    while True:
+        cm.dict['played'] = cm.dict['played'] + 1
+        line = int(input("Informe a linha:"))
+        column = int(input("Informe a coluna:"))
+
+        cm.dict['line'] = line
+        cm.dict['column'] = column
+        cm.dict['played'] += cm.dict['played']
+
+        request = cm.dict
+        request = str(request)
+        request = request.encode(ENCODE)				   
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+        dest = (HOST, PORT)                                     
+        sock.sendto(request, dest)                                
+
+        subprocess.call('cls', shell=True)
+    
+        answer, address = sock.recvfrom(MAX_BYTES)    # Recebendo dados
+        answer = answer.decode(ENCODE)                  # Convertendo dados de BASE64 para UTF-8
+        answer = ast.literal_eval(answer)
+
+        if(answer['altered'] == True):
+            cm.updateDict(answer)
+            cm.showCleanField()
+            print(answer['msg'])
+            print('Falta descobrir', answer['freeAreas'], 'áreas')
+        else:
+            cm.showCleanField()
+            print(answer['msg'])
+            print('Falta descobrir', answer['freeAreas'], 'áreas')
+
+        if(answer['controlPlay'] == 2):
+            print('/n')
+            print('/n')
+            print('/n')
+            A = int(input('Digite 1 se quer jogar novamente'))
+        sock.close()
+    client()        
+
+client()
+
+
+    
+        
+
---
+++ trabVP2/questao02/server/serverModel.py
@@ -0,0 +1,37 @@
+import random
+
+class Field:
+    
+    def __init__(self, fieldSize, numberBomb):
+        self.numberBomb = numberBomb
+        self.cleanField = [['-' for i in range(fieldSize)] for i in range(fieldSize)]
+        self.mineField = self.generateBomb([[0 for i in range(fieldSize)] for i in range(fieldSize)], self.numberBomb)
+        self.dict = self.createDictionary({}, fieldSize)
+    
+    def createDictionary(self, dict, fieldSize):
+        for x in range(fieldSize):
+            for y in range(fieldSize):
+                dict[(x, y)] = '-'
+
+        dict['msg'] = 'Inicio'
+        dict['freeAreas'] = (fieldSize * fieldSize - self.numberBomb)
+        dict['played'] = 0
+        dict['altered'] = True
+        dict['contolPlay'] = 0
+        dict['id'] = 0
+        return dict
+
+    def generateBomb(self, cleanField, numberBomb):    
+        numberBomb = numberBomb
+        fieldSize = len(cleanField)
+        mineField = cleanField
+        while numberBomb > 0:
+            x = random.randint(0, fieldSize - 1)
+            y = random.randint(0, fieldSize - 1) 
+            for i in mineField:
+                if(mineField[x][y] == 9):
+                    continue
+                else:
+                    mineField[x][y] = 9
+                    numberBomb = numberBomb - 1
+        return mineField    
