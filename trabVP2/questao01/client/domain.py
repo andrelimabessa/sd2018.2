2b7fc51e9e4ccf3b83147af1f977294d69df1f1b
Davi Costa <davispcosta@gmail.com>
2018-11-11 22:55:24 -0300

    tirando commit do canto errado

--- trabVP2/questao01/client/domain.py
+++
@@ -1,7 +0,0 @@
-class Field:
-
-    def __init__(self, fieldSize):
-        self.cleanField = [['-' for i in range(fieldSize)] for i in range(fieldSize)]
-        self.dict = {'line': 0, 'column': 0, 'played': 0}
-
-
